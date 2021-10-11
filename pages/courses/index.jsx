e44fce0c2248ae7386a98ff04233de6ac09a5919
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2021-10-10 19:56:10 -0500

    add new course page

--- pages/courses/index.jsx
+++ pages/courses/index.jsx
@@ -1,5 +1,6 @@
 import { getSession } from 'next-auth/react'
 import Link from 'next/link'
+import { PlusIcon } from '@heroicons/react/solid'
 
 import { prisma } from '@lib/db'
 import Layout from '@components/layout'
@@ -10,9 +11,19 @@ export default function Courses({ user, courses }) {
   return (
     <Layout user={user} title="courses">
       <PageAnimate classes="w-full h-full flex flex-col bg-gray-50 items-center justify-left space-y-5 overflow-scroll">
-        <h1 className="text-5xl mt-12 ml-12 font-bold text-gray-800 w-full justify-start">
-          Courses
-        </h1>
+        <div className="flex w-full items-center justify-center ">
+          <h1 className="text-5xl mt-12 ml-12 font-bold text-gray-800 w-full">
+            Courses
+          </h1>
+          {user.role === 'advisor' && (
+            <Link href="/courses/new" passHref>
+              <div className="flex items-center space-x-3 mr-10 text-red-800 transition px-4 py-2 rounded-lg font-bold shadow hover:text-gray-800 hover:shadow-xl cursor-pointer">
+                <span>new</span>
+                <PlusIcon width={30} />
+              </div>
+            </Link>
+          )}
+        </div>
         <div className="w-full p-10 flex flex-col md:flex-row items-center justify-center flex-wrap space-y-8 md:space-x-12">
           {courses.map((course, i) => (
             <CourseCard course={course} key={i} />
